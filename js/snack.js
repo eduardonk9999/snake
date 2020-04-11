efa45b0c1bfd65fc547824f5ab904ae522ffd1fc
Eduardo <thiago.novak7@gmail.com>
2020-04-11 12:33:35 -0300

    Add, project initial

---
+++ js/snack.js
@@ -0,0 +1,29 @@
+class Snake(){
+  this.stage = stage;
+  this.context = context;
+  this.velocity = velocity;
+  //velocidade inicial
+  this.velocityX = velocityX;
+  this.velocityY = velocityY;
+  //posição inicial Snake
+  this.positionX = positionX;
+  this.positionY = positionY;
+
+  //Tamanho do CUBO
+  this.cube = cube;
+  //Quantidade de cubos no canvas (aumount)
+  this.aumountC = aumountC;
+
+  //posição da Maçã
+  this.appleX = appleX;
+  this.appleY = appleY;
+
+  //rastro da snake
+  this.trail = trail;
+  
+  //rabo
+  this.tail = tail;
+
+}
+
+
